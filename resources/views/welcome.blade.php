f1e26d6057bb9e4c4287d60dc42b881e8e6cd971
egpheel <eduardo.gil@gmail.com>
2016-06-01 19:43:13 +0100

    sassifying

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,44 +3,15 @@
     <head>
         <title>TV Shows Project</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="/css/app.css" media="screen" title="no title" charset="utf-8">
 
         <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-                @if (isset($tvshow))
-                  background-image: url({{ $tvshow->fanart }});
-                  background-size: cover
-                @endif
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-
-            .info {
-                font-size: 42px;
-            }
+          body {
+            @if (isset($tvshow))
+              background-image: url({{ $tvshow->fanart }});
+              background-size: cover;
+            @endif
+          }
         </style>
     </head>
     <body>
